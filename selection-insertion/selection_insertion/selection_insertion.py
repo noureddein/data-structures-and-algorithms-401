81d2ec529d049004e2ead6f6aea63b17e17699ea
noureddein <noureddein@gmail.com>
2022-05-16 21:50:33 +0300

    Update README file

--- selection-insertion/selection_insertion/selection_insertion.py
+++ selection-insertion/selection_insertion/selection_insertion.py
@@ -11,17 +11,17 @@ def validate_items(_list):
 
 
 def selection_sort(arr):
-    if validate_items(arr):
-        return
+    validate_items(arr)
     n = len(arr)
-    for i, val1 in enumerate(arr):
+    for i, val1 in enumerate(arr):  # for (i=0, condition, i++) for i in len(arr)
+        # arr[i]
         min = i
         j = i
         for _ in arr:
-            j += 1
+            # j += 1
             if j > n - 1:
                 break
-            if arr[j] < arr[min]:
+            if arr[i + 1] < arr[min]:
                 min = j
         temp = arr[min]
         arr[min] = arr[i]
@@ -31,4 +31,4 @@ def selection_sort(arr):
 
 if __name__ == "__main__":
     print(selection_sort([2, 3, 5, 7, 13, 1]))
-    print(selection_sort([]))
+    # print(selection_sort([]))
